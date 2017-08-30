7fdcbe546fabfbf53fbc3ad3616224cfd44d6315
DESKTOP-G7JTIU0\victor <vmdlssnt@gmail.com>
2017-08-30 17:22:41 -0500

    listofplayers on the client is now updating correctly when a player joins or leaves.

--- vicsparty/vicsparty/Utilities/mywebsocket.cs
+++ vicsparty/vicsparty/Utilities/mywebsocket.cs
@@ -6,6 +6,7 @@ using System.Web;
 using System.Web.Helpers;
 using Microsoft.Web.WebSockets;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using vicsparty.Utilities;
 using vicsparty.Utilities.GameObjects;
 
@@ -23,22 +24,34 @@ namespace MacPortafolio.Utilities
             mywebsocketcollection.Add(this);
         }
 
+        private void updatewebsocketcollection(string typeofalert)
+        {
+            JArray listofplayers = new JArray();
+
+            foreach (var element in clients)
+            {
+                //joinalertmessage newjoinalert = new joinalertmessage();
+                //newjoinalert.sender = ((mywebsocket)element).name;
+                //newjoinalert.type = "joinalert";
+                listofplayers.Add(((mywebsocket)element).name);
+                //clients.Broadcast(JsonConvert.SerializeObject(newjoinalert));
+            }
+            JObject o =new JObject();
+            o["listofplayers"] = listofplayers;
+            o["typeofalert"] = typeofalert;
+            clients.Broadcast(o.ToString());
+        }
         public override void OnOpen()
         {
             name = this.WebSocketContext.QueryString["name"];
             clients.Broadcast(name + " has connected to room "+ clients.collectionID);
             //broadcast json object with player who just connected
-            
-           
-            foreach (var element in clients)
-            {
-                joinalertmessage newjoinalert = new joinalertmessage();
-                newjoinalert.sender = ((mywebsocket)element).name;
-                newjoinalert.type = "joinalert";
-                clients.Broadcast(JsonConvert.SerializeObject(newjoinalert));
-            }
+            updatewebsocketcollection("joinalert");
 
-            
+
+
+
+
         }
 
         public override void OnMessage(string message)
@@ -51,7 +64,7 @@ namespace MacPortafolio.Utilities
         {
             clients.Remove(this);
             clients.Broadcast(string.Format("{0} has gone away ", name));
-
+            updatewebsocketcollection("leavealert");
         }
 
         
